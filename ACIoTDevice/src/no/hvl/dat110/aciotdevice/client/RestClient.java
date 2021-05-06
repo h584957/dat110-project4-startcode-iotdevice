0398571173f60299b23e653070d9a62afa0532b5
h584957 <54099686+h584957@users.noreply.github.com>
2021-05-06 16:02:17 +0200

    Koding ferdig laget og testet. Fullført

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -21,12 +21,20 @@ public class RestClient {
 	public void doPostAccessEntry(String message) {
 
 		// TODO: implement a HTTP POST on the service to post the message
+			
+	        String jsonString = new Gson().toJson(new AccessMessage(message));
+	        
+	        RequestBody body = RequestBody.create(MediaType.parse("application/json"), jsonString);
 
-		Gson gson = new Gson();
-		
-		RequestBody body = RequestBody.create(MediaType.parse(logpath), message);
-		
-		Request request = new Request.Builder().url("http://localhost:8080" + logpath).post(body).build();
+	        OkHttpClient client = new OkHttpClient();
+
+	        Request req = new Request.Builder().url("http://localhost:8080" + logpath).post(body).build();
+
+	        try (Response res = client.newCall(req).execute()) {
+	            System.out.println(res.body().string());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 		
 	}
 
@@ -35,7 +43,7 @@ public class RestClient {
 	public AccessCode doGetAccessCode() {
 
 		AccessCode code = null;
-
+		
 		// TODO: implement a HTTP GET on the service to get current access code
 		OkHttpClient client = new OkHttpClient();
 		Gson gson = new Gson();
